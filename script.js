716c44a116740277cba40ab7b3b08e5939cf5993
Jacopo <jakonigro@gmail.com>
2020-08-06 15:57:45 +0200

    fine esercizio

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ var nome = prompt("come ti chiami?");
 // chiedo sesso
 var sesso = prompt("sei uomo o donna?");
 // stampo nome
-if (sesso = "uomo") {
+if (sesso == "uomo") {
   document.getElementById("name").className = "lightblue";
+  document.getElementById("name").innerHTML = nome;
 }
-else if (sesso = "donna") {
+else if (sesso == "donna") {
   document.getElementById("name").className = "pink";
+  document.getElementById("name").innerHTML = nome;
 }
-document.getElementById("name").innerHTML = nome;
+else {
+  document.getElementById("name").innerHTML = "inserisci un sesso valido!";
+
+}
